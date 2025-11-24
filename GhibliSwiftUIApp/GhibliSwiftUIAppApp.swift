0d9195974eb1fa2c3ddf8ae0240f888bf46f0c00
Thais Menezes <thais.mas@hotmail.com>
2025-11-24 11:36:05 -0300

    Initial Commit

---
+++ GhibliSwiftUIApp/GhibliSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUIAppApp.swift
+//  GhibliSwiftUIApp
+//
+//  Created by Thais Souza on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GhibliSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GhibliSwiftUIAppTests/GhibliSwiftUIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUIAppTests.swift
+//  GhibliSwiftUIAppTests
+//
+//  Created by Thais Souza on 24/11/25.
+//
+
+import Testing
+@testable import GhibliSwiftUIApp
+
+struct GhibliSwiftUIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
